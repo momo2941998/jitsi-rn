f866ea715ab0d415f7e133bff88f8f9fa78fa8b1
Nguyen Quang Thanh <thanh.nguyenquang@mobifone.vn>
2022-05-05 15:24:00 +0700

    add jitsi success

--- App.js
+++ App.js
@@ -1,112 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useEffect } from 'react';
+import JitsiMeet, { JitsiMeetView } from 'react-native-jitsi-sdk';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  useEffect(() => {
+    setTimeout(() => {
+      const url = 'https://meet.jit.si/exemple';
+      const userInfo = {
+        displayName: 'User',
+        email: 'user@example.com',
+        avatar: 'https:/gravatar.com/avatar/abc123',
+      };
+      JitsiMeet.call(url, userInfo);
+      /* Você também pode usar o JitsiMeet.audioCall (url) para chamadas apenas de áudio */
+      /* Você pode terminar programaticamente a chamada com JitsiMeet.endCall () */
+    }, 1000);
+  }, [])
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    return () => {
+      JitsiMeet.endCall();
+    };
+  });
+
+  function onConferenceTerminated(nativeEvent) {
+    /* Conference terminated event */
+    console.log(nativeEvent)
+  }
+
+  function onConferenceJoined(nativeEvent) {
+    /* Conference joined event */
+    console.log(nativeEvent)
+  }
+
+  function onConferenceWillJoin(nativeEvent) {
+    /* Conference will join event */
+    console.log(nativeEvent)
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+    <JitsiMeetView
+      onConferenceTerminated={e => onConferenceTerminated(e)}
+      onConferenceJoined={e => onConferenceJoined(e)}
+      onConferenceWillJoin={e => onConferenceWillJoin(e)}
+      style={{
+        flex: 1,
+        height: '100%',
+        width: '100%',
+      }}
+    />
+  )
+}
 export default App;
--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,13 @@
  * @format
  */
 
+const blacklist = require('metro-config/src/defaults/exclusionList');
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      /ios\/Pods\/JitsiMeetSDK\/Frameworks\/JitsiMeet.framework\/assets\/node_modules\/react-native\/.*/,
+    ]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
